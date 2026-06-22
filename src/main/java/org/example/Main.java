dc24b8b83aaf54329972cd51809c11a74ffea65b
ligenqiang <ligenqiang2026@gmail.com>
2026-06-22 09:50:51 +0800

    Revert "Reapply "0623 tijiao""
    This reverts commit cc8a7ba817ac70569706568e7a752c9e791e43da.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,6 +19,6 @@ public class Main {
         }
         
         System.out.println("========================");
-        System.out.println("0623");
+        System.out.println("0622");
     }
 }
